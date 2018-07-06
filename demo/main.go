a56abdc0a86edb37e22e92028c67ecafa9dc861b
orange soeur <1034236490@qq.com>
2018-07-06 16:32:09 +0800

    Update and rename mian.go to main.go

--- demo/main.go
+++ demo/main.go
@@ -16,6 +16,7 @@ func Initialize(autocode int32) int32 {
 	return cqsdk_golang.EVENT_IGNORE
 }
 
+//此处注释与函数名字必须跟appid.json里所填写的一模一样
 //export OnEnable
 func OnEnable() int32 {
 	cqsdk_golang.AddLog(cqsdk_golang.CQLOG_INFO, "启动", "test插件成功启动")
